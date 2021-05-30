9deda6a64acc75aea53403b3e6522fd332f0159e
danielchih1021 <chi17016@byui.edu>
2021-05-30 01:06:23 -0400

    commit

--- todo/todos.js
+++ todo/todos.js
@@ -2,25 +2,92 @@ import {Todo} from './todo.js';
 
 let toDoList = [];
 
-let todo1= new Todo('Mow the lawn');
-let todo2= new Todo('Do the dishes');
-let todo3= new Todo('Visit grandma');
-let todo4= new Todo('Sweeping');
+if(localStorage.getItem('toDoList')){
+   toDoList = JSON.parse(localStorage.getItem('toDoList'));      
+}
 
-toDoList.push(todo1);
-toDoList.push(todo2);
-toDoList.push(todo3);
-toDoList.push(todo4);
+displayTodos(toDoList);
 
-let ul = document.querySelector('ul');
-
-toDoList.forEach(
-    todoItem => {
+function displayTodos(todos) {
+    let ul = document.querySelector('ul');
+    ul.innerHTML="";
+    todos.forEach((todoItem)=>{
         ul.innerHTML += 
-            `<li> 
-                <input type="checkbox" id="">
-                <span>${ todoItem.Content }</span>
-                <button>X</button>
-            <li>`;
-    }
-);
+                    `<li> 
+                        <input type="checkbox" data-id="${todoItem.Id}" ${todoItem.Completed ? 'checked' : ''}>
+                        <span>${ todoItem.Content }</span>
+                        <button data-id="${todoItem.Id}"}>X</button>
+                    <li>`;
+    });
+
+    let todoCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    todoCheckboxes.forEach(
+        todoCheckbox => {
+            todoCheckbox.addEventListener('touchend', (e) => {
+                let selectedId = e.target.dataset.id;
+                let selectedTodo = toDoList.find(todo => todo.Id === parseInt(selectedId));
+                
+                selectedTodo.Completed = !selectedTodo.Completed;
+
+                localStorage.setItem('toDoList', JSON.stringify(toDoList));
+                console.log(selectedTodo);
+                let countNotCompleted = 0;
+                toDoList.forEach(
+                    (toDoListItem) => {
+                        if (toDoListItem.Completed == false){
+                            countNotCompleted += 1;
+                        }
+                    }
+                )
+    document.querySelector("#countTask").innerHTML= countNotCompleted+" tasks left";
+            });
+        });
+
+    //ToDO: add event listener for each button to delete
+    let todoDeleteButtons = document.querySelectorAll('button');
+
+    todoDeleteButtons.forEach((todoDeleteButton) => {
+        todoDeleteButton.addEventListener('touchend', (e) => {
+            let selectedId = e.target.dataset.id;
+            let selectedTodoIndex = toDoList.findIndex(todo => todo.Id === parseInt(selectedId)); 
+            
+            // delete object from the array
+            toDoList.splice(selectedTodoIndex, 1);
+
+            localStorage.setItem("toDoList", JSON.stringify(toDoList));
+
+
+            displayTodos(toDoList);
+        });
+    });
+
+    let countNotCompleted = 0;
+    toDoList.forEach(
+        (toDoListItem) => {
+            if (toDoListItem.Completed == false){
+                countNotCompleted += 1;
+            }
+        }
+    )
+    document.querySelector("#countTask").innerHTML= countNotCompleted+" tasks left";
+}
+
+
+let add = document.querySelector("#add");
+add.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    let input = document.querySelector("#newTask");
+
+    let newToDoItem = new Todo(input.value);
+    toDoList.push(newToDoItem);
+    input.value="";
+    
+    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    displayTodos(toDoList);
+});
+
+document.querySelector("#all").addEventListener("touchend", displayTodos(toDoList));
+document.querySelector("#active").addEventListener("touchend", displayTodos(toDoList.filter(todo => todo.Completed == false)));
+document.querySelector("#completed").addEventListener("touchend", displayTodos(toDoList.filter(todo => todo.Completed == true)));
+
