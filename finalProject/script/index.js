758d151a1150f46b6eeec0e9760a104a65742e53
danielchih1021 <chi17016@byui.edu>
2021-07-15 12:46:25 -0500

    commit

--- finalProject/script/index.js
+++ finalProject/script/index.js
@@ -1,41 +1,59 @@
 import { Food } from "./food.js";
-import { Ingredient} from "./ingredient.js";
+import { Ingredient } from "./ingredient.js";
+import { Nutrients } from "./nutrient.js";
 
 let foods = [];
-let unsoldFoods=[];
-let ingredients=[];
-if(localStorage.getItem('foodList')){
-    foods = JSON.parse(localStorage.getItem('foodList'));     
-    for (let i=0; i<foods.length;i++){
-        if(foods[i].Sold = false){
-            unsoldFoods.push(foods[i]);
-        }
+let unsoldFoods = [];
+let ingredients = [];
+if (localStorage.getItem("foodList")) {
+  foods = JSON.parse(localStorage.getItem("foodList"));
+  for (let i = 0; i < foods.length; i++) {
+    if ((foods[i].Sold = false)) {
+      unsoldFoods.push(foods[i]);
     }
-    displayFood(unsoldFoods);
+  }
+  displayFood(unsoldFoods);
 }
 
 //To display the food available for sale
-function displayFood(unsoldFoodArray){
-    let selection = document.querySelector("section");
-    selection.innerHTML = "";
-    let foodPost = "";
-    unsoldFoodArray.forEach((fooditem)=>{
-        foodPost = foodPost + fooditem.Title + "<br>" 
-            fooditem.Category + "<br>" + 
-            fooditem.Description;
-        fooditem.Ingredient.forEach((ingredientItem) =>{
-            let i = 1;
-            foodPost = foodPost + "Ingredient #" + i + " " + ingredientItem.Name + " serving: " + ingredientItem.Serving + "<br>";
-            i++;
-        })
-        selection.innerHTML +=
-            `<div class="foodSelection" data-id="${fooditem.TimeId}">
+function displayFood(unsoldFoodArray) {
+  let selection = document.querySelector("section");
+  selection.innerHTML = "";
+  let foodPost = "";
+  unsoldFoodArray.forEach((fooditem) => {
+    let foodNumber = unsoldFoodArray.length;
+    console.log(fooditem.Description);
+    console.log(fooditem.Category);
+    foodPost =
+      "Food #" +
+      foodNumber +
+      "<br>" +
+      fooditem.Title +
+      "<br>" +
+      fooditem.Category +
+      "<br>" +
+      fooditem.Description;
+    let i = 1;
+    fooditem.Ingredient.forEach((ingredientItem) => {
+      foodPost =
+        foodPost +
+        "Ingredient #" +
+        i +
+        " " +
+        ingredientItem.Name +
+        " serving: " +
+        ingredientItem.Serving +
+        "<br>";
+      i++;
+    });
+    selection.innerHTML += `<div class="foodSelection" data-id="${fooditem.TimeId}">
                 ${foodPost}<br>
                 <span>Purchase This</span>
-                <input type="checkbox" date-id="${fooditem.TimeId}" name = "purchase">
+                <input type="checkbox" date-id="${fooditem.TimeId}" name = "purchase"><br>
+                <input type="button" data-id="${fooditem.TimeId}" name = "show_nutrients" value = "Show top three nutrients">
             </div>`;
-        foodPost = ""; 
-    })
+    foodPost = "";
+  });
 }
 
 //Actions performed when clicking the sell your food button
@@ -46,117 +64,175 @@ document.querySelector("button").addEventListener("touchend", () => {
   let selectedCategory;
   let radios = document.getElementsByName("food-category");
   for (let i = 0; i < radios.length; i++) {
-      if(radios[i].checked){
-          selectedCategory = radios[i].value;
-          break;
-      }
+    if (radios[i].checked) {
+      selectedCategory = radios[i].value;
+      break;
+    }
   }
-  if(title == "" || description == "" || typeof selectedCategory === "undefined" || ingredients === []){
-      alert("Title, food category, ingredient and descriptions are required");
+  if (
+    title == "" ||
+    description == "" ||
+    typeof selectedCategory === "undefined" ||
+    ingredients === []
+  ) {
+    alert("Title, food category, ingredient and descriptions are required");
   } else {
-  let food = new Food(title, selectedCategory, ingredients, description, "../finalProject/images/food.jpg");
-  ingredients = [];
-  foods.push(food);
-  console.log(foods);
-  localStorage.setItem("foodList", JSON.stringify(foods));
-  unsoldFoods = [];
-  for (let i=0; i<foods.length;i++){
-      if(foods[i].Sold == false){
-          unsoldFoods.push(foods[i]);
+    let food = new Food(
+      title,
+      selectedCategory,
+      ingredients,
+      description,
+      "../finalProject/images/food.jpg"
+    );
+    ingredients = [];
+    foods.push(food);
+    console.log(foods);
+    localStorage.setItem("foodList", JSON.stringify(foods));
+    unsoldFoods = [];
+    for (let i = 0; i < foods.length; i++) {
+      if (foods[i].Sold == false) {
+        unsoldFoods.push(foods[i]);
       }
-  }
-  document.querySelector("#title").value = "";
-  document.querySelector("#ingredient").value = "";
-  document.querySelector("#description").value = "";
-  for (let i = 0; i < radios.length; i++) {
-    if(radios[i].checked){
+    }
+    document.querySelector("#title").value = "";
+    document.querySelector("#ingredient").value = "";
+    document.querySelector("#description").value = "";
+    for (let i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
         radios[i].checked = false;
         break;
+      }
     }
-}
-//Clear all the input textbox values
-let textboxes = document.querySelectorAll('input[type="text"]');
-for(let i = 0; i<textboxes.length;i++){
-    console.log(textboxes[i].value);
-    textboxes[i].value = "";
-}
-
-if (textboxes.length>3){
-    do{
-        document.querySelectorAll('input[type="text"]')[3].remove();
+    //Clear all the input textbox values
+    let textboxes = document.querySelectorAll('input[type="text"]');
+    for (let i = 0; i < textboxes.length; i++) {
+      console.log(textboxes[i].value);
+      textboxes[i].value = "";
     }
-    while(document.querySelectorAll('input[type="text"]')[3]);
-}
 
-//Removing all delete ingredient buttons after submitting the food to sell
-// if(document.querySelectorall('input[data-id=""]').length>0){
-//     do{
-//         document.querySelectorall('input[type="button"][value="Delete Ingredient"]')[0].remove();
-//     }
-//     while(document.querySelectorall('input[type="button"][value="Delete Ingredient"]')[0]);
-// }
-// let delete_ingredient_buttons = document.querySelectorall('input[type="button"][class="ingredient_delete_button"]');
-// console.log(delete_ingredient_buttons);
-
-  displayFood(unsoldFoods);
-}});
-
-//Actions performed after clicking the add ingredient button
-document.querySelector("#ingredient_added_button").addEventListener("touchend", () => {
-    let added_ingredient = document.querySelector("#ingredient").value;
-    let added_serving = document.querySelector("#serving").value;
-    if(added_ingredient == "" || added_serving == ""){
-        alert("Ingredient and serving are both required")
-    }else {
-    let ingredient = new Ingredient(added_ingredient, added_serving);
-    document.querySelector(".ingredient_div").innerHTML +=
-        `<input type="text" name="ingredient" class="added_ingredient" value="${added_ingredient}" maxlength="6" size="6" data-id="${ingredient.TimeId}" readonly>
-        <input type="text" name="serving" class="added_serving" value="${added_serving}" maxlength="6" size="6" data-id="${ingredient.TimeId}" readonly>
-        <input type="button" value="Delete Ingredient" name="delete" class="ingredient_delete_button" data-id="${ingredient.TimeId}"<br>`;
-        document.querySelector("#ingredient").value = "";
-    document.querySelector("#serving").value = "";
-    ingredients.push(ingredient);
-    console.log(ingredients);}
-
-//Actions performed after hitting the delete ingredient button
-let delete_ingredient_buttons = document.querySelectorAll('.ingredient_delete_button');
-console.log(delete_ingredient_buttons);
-delete_ingredient_buttons.forEach(
-    delete_ingredient_button => {
-        delete_ingredient_button.addEventListener('touchend', (e) => {
-            let selectedId = e.target.dataset.id;
-            console.log(selectedId);
-            for(let i = 0; i<ingredients.length;i++){
-                let selectedIngredient = ingredients.find(ingredient => ingredient.TimeId === parseInt(selectedId));
-                if(selectedIngredient){
-                    console.log(selectedIngredient);
-                }
-            }
-
-            // selectedTodo.Completed = !selectedTodo.Completed;
-
-            // localStorage.setItem('toDoList', JSON.stringify(toDoList));
-            // let countNotCompleted = 0;
-            // toDoList.forEach(
-            //     (toDoListItem) => {
-            //         if (toDoListItem.Completed == false){
-            //             countNotCompleted += 1;
-            //         }
-            //     }
-            // )
-        });
+    // Removing all input tag with a dataset id after submitting the food to sell
+    document.querySelectorAll("input").forEach((inputItem) => {
+      if (inputItem.dataset.id) {
+        inputItem.remove();
+      }
     });
-})
 
-//Fetch API to reference to external API
-const params={
-    api_key: 'TYiTH5rSuMu3C72sXI27po6nP1gAdP0YQcqghQI0',
-    query:'egg',
+    displayFood(unsoldFoods);
+
+    document
+      .querySelectorAll('input[name="show_nutrients"]')
+      .forEach((showNutrientButton) => {
+        showNutrientButton.addEventListener("touchend", (e) => {
+          let selectedId_nutrient = e.target.dataset.id;
+          for (let i = 0; i < unsoldFoods.length; i++) {
+            let targetedFood_nutrient = unsoldFoods.find(
+              (ingredient) =>
+                ingredient.TimeId === parseInt(selectedId_nutrient)
+            );
+            console.log(targetedFood_nutrient);
+            extractNutrientValue(targetedFood_nutrient);
+          }
+        });
+      });
+  }
+});
+
+function extractNutrientValue(targetedFood) {
+  //Fetch API to reference to external API
+  const params = {
+    api_key: "TYiTH5rSuMu3C72sXI27po6nP1gAdP0YQcqghQI0",
+    query: "rice",
     dataType: ["Survey (FNDDS)"],
     pagesize: 5,
+  };
+
+  const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${params.query}&pageSize=${params.pagesize}&dataType=${params.dataType}&api_key=${params.api_key}`;
+  fetch(api_url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
-const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${params.query}&pageSize=${params.pagesize}&dataType=${params.dataType}&api_key=${params.api_key}`;
-fetch(api_url)
-  .then(response => response.json())
-  .then(data => console.log(data));
+function displayIngredient(ingredientList) {
+  ingredientList.forEach((ingredientUnit) => {
+    document.querySelector(
+      ".ingredient_div"
+    ).innerHTML += `<div data-id="${ingredientUnit.TimeId}">
+            <input type="text" value="${ingredientUnit.Name}" maxlength="6" size="6" data-id="${ingredientUnit.TimeId}">
+            <input type="text" value="${ingredientUnit.Serving}" maxlength="6" size="6" data-id="${ingredientUnit.TimeId}">
+            <input type="button" value="Update" class="ingredient_update_button" data-id="${ingredientUnit.TimeId}">
+            <input type="button" value="Delete" class="ingredient_delete_button" data-id="${ingredientUnit.TimeId}"><br>
+        </div>`;
+  });
+}
+
+//Actions performed after clicking the add ingredient button
+document
+  .querySelector("#ingredient_added_button")
+  .addEventListener("touchend", () => {
+    let added_ingredient = document.querySelector("#ingredient").value;
+    let added_serving = document.querySelector("#serving").value;
+    console.log(added_ingredient);
+    console.log(added_serving);
+    if (added_ingredient == "" || added_serving == "") {
+      alert("Ingredient and serving are both required");
+    } else {
+      let ingredient = new Ingredient(added_ingredient, added_serving);
+      document.querySelector("#serving").value = "";
+      document.querySelector("#ingredient").value = "";
+      ingredients.push(ingredient);
+      console.log(ingredients);
+      displayIngredient(ingredients);
+    }
+
+    //Actions performed after hitting the edit ingredient button
+    let edit_ingredient_buttons = document.querySelectorAll(
+      ".ingredient_update_button"
+    );
+    edit_ingredient_buttons.forEach((edit_ingredient_button) => {
+      edit_ingredient_button.addEventListener("touchend", (e) => {
+        let seletedEditId = e.target.dataset.id;
+        console.log(seletedEditId);
+        for (let i = 0; i < ingredients.length; i++) {
+          let selectedIngredient_edit = ingredients.find(
+            (ingredient) => ingredient.TimeId === parseInt(seletedEditId)
+          );
+          console.log(selectedIngredient_edit);
+          let selected_index = ingredients.indexOf(selectedIngredient_edit);
+          let editedIngredient = document.querySelector(
+            `'input[value="${selectedIngredient_edit.Name}"]'`
+          ).value;
+          ingredients[selected_index].Name = editedIngredient;
+          let editedServing = document.querySelector(
+            `'input[value="${selectedIngredient_edit.Serving}"]'`
+          ).value;
+          ingredients[selected_index].Serving = editedServing;
+          alert(
+            "Ingredient: " +
+              editedIngredient +
+              " and serving: " +
+              editedServing +
+              " have been updated"
+          );
+        }
+      });
+    });
+
+    //Actions performed after hitting the delete ingredient button
+    let delete_ingredient_buttons = document.querySelectorAll(
+      ".ingredient_delete_button"
+    );
+    delete_ingredient_buttons.forEach((delete_ingredient_button) => {
+      delete_ingredient_button.addEventListener("touchend", (e) => {
+        let selectedId = e.target.dataset.id;
+        console.log(selectedId);
+        for (let i = 0; i < ingredients.length; i++) {
+          let selectedIngredient = ingredients.find(
+            (ingredient) => ingredient.TimeId === parseInt(selectedId)
+          );
+          console.log(selectedIngredient);
+          ingredients.splice(selectedIngredient);
+          console.log(ingredients);
+        }
+      });
+    });
+  });
---
+++ finalProject/script/nutrient.js
@@ -0,0 +1,8 @@
+export class Nutrients{
+    constructor(timeId, protein, fat, carb){
+        this.TimeId = timeId;
+        this.Protien = protein;
+        this.Fat = fat;
+        this.Carb = carb;
+    }
+}
